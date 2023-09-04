5fdfdf3d434688c4a83267ef052b8cb6df6a5848
TheMarksniper <marekraskah@gmail.com>
2023-09-04 16:18:18 +0200

    changes to multi nav, working only for one robot, needs fixing

--- src/swarm/swarm_gazebo/launch/swarm_nav_launch.py
+++ src/swarm/swarm_gazebo/launch/swarm_nav_launch.py
@@ -16,35 +16,32 @@ from launch_ros.substitutions import FindPackageShare
 find_robot_namespaces = "ros2 topic list | grep --only-matching 'linorobot2_[^/]*' | sort --unique"
 #os.system(find_robot_namespaces)
 result = subprocess.getoutput(find_robot_namespaces)
-print(result)
-namespace = result.split("\n")
+print(len(result))
+namespaces = result.split("\n")
 
 def generate_launch_description():
     use_sim_time = True
+    DeclareLaunchArgument(
+            'namespace', default_value='',
+             description='Top-level namespace')
     navigation_launch_path = PathJoinSubstitution(
         [FindPackageShare('swarm_gazebo'), 'launch', 'nav_launch.py']
     )
-    pkg_swarm_description = get_package_share_directory('swarm_description')
-
     spawn_nav_cmds = []
-    for robot in namespace:
-        
-
+    for namespace in namespaces:
         spawn_nav_cmds.append(
             PythonLaunchDescriptionSource(navigation_launch_path),
             launch_arguments={
-                'namespace': robot 
+                'namespace':namespace,
+                'use_sime_time':use_sim_time
             }.items()
                 
         )
-
+    print()
     # Create the launch description and populate
     ld = LaunchDescription()
-    
+    #
     for spawn_nav_cmd in spawn_nav_cmds:
-        ld.add_action(spawn_nav_cmd)
+        ld.add_action(spawn_nav_cmd)      
 
     return ld
-
-if __name__ == "__main__":
-    generate_launch_description()
